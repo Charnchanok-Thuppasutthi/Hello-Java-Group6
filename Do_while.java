2522cdaf0b7746b155811f8644d814aa0d7f8837
Nipon kamfoy <s6201012630061@email.kmutnb.ac.th>
2020-08-17 21:10:58 +0700

    practice to use do while

---
+++ Do_while.java
@@ -0,0 +1,12 @@
+class Do_while {
+	public static void main (String[] args){
+		int i = 0 ;
+		/*It will do in the block first then it will check condition 
+		  later */
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while (i <= 10);	
+	}
+}
