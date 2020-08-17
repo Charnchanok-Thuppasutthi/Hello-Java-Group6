474f13d7fb96b88c0b05dbf8a480f7722ac47fa4
pakpoom <s6201012620210@email.kmutnb.ac.th>
2020-08-17 17:16:25 +0700

    add variable and type

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,4 +1,4 @@
-class HelloWorldApp { 
+class HelloWorldApp {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("My first time with Java");
---
+++ variableandtype.java
@@ -0,0 +1,21 @@
+class variableandtype{
+	public static void main(String[] args){
+		// datatype and variablename 
+		boolean Yes = true; // boolean
+		char y = 'y'; // character
+		int num1 = 3; // integer
+		float num2 = 1.01f; // float
+		double num3 = 3.1; //double
+		String name = "pakpoom"; // string
+		int[] num_list = {1,2,3,4,5,6,7,8,9}; //array integer
+
+		System.out.println(Yes);
+		System.out.println(y);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(name);
+		System.out.println(num_list[8]);
+		
+	}
+}
