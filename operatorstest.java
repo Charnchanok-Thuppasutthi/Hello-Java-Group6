63322e60e0de2892cbcef5059a0d7adaf556688a
pakpoom <s6201012620210@email.kmutnb.ac.th>
2020-08-17 17:56:13 +0700

    add operators test

---
+++ operatorstest.java
@@ -0,0 +1,16 @@
+class operatorstest{
+	public static void main(String[] args){
+		int num1 = 5;
+		float num2 = 10f;
+		// try operators
+		System.out.println(num1*num2); // *
+		System.out.println(num1/num2); // / 
+		System.out.println(num1%num2); // %
+		System.out.println(num1+num2); // +
+		System.out.println(num1-num2); // -
+		System.out.println(num1<num2); // <
+		System.out.println(num1>num2); // >
+		System.out.println(num1==num2); // ==
+		System.out.println(num1!=num2); // !=
+	}
+}
